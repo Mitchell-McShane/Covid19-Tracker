2bad5b01ec6fdd55c8857338dbd4d5d148c61250
Riph Kitsune <mitchell.mcshane1987@gmail.com>
2021-02-21 17:51:38 +0000

    Fixed up some CSS styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { MenuItem, FormControl, Select, } from '@material-ui/core';
+import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core';
 import InfoBox from './InfoBox';
+import Map from './Map';
 
 function App() {
 
@@ -34,29 +35,38 @@ function App() {
 
   return (
     <div className="App">
-      <div className="app-header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app_dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
+      <div className="app-left">
+        <div className="app-header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
 
 
 
-          </Select>
-        </FormControl>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app-stats">
+          <InfoBox title="Coronavirus Cases" cases={136} total={2000} />
+
+          <InfoBox title="Recovered" cases={1364} total={3000} />
+
+          <InfoBox title="Deaths" cases={1362} total={4000} />
+        </div>
+
+        <Map />
       </div>
-
-      <div className="app-stats">
-        <InfoBox title="Coronavirus Cases" cases={136} total={2000} />
-
-        <InfoBox title="Recovered" cases={1364} total={3000} />
-
-        <InfoBox title="Deaths" cases={1362} total={4000} />
-      </div>
-
+      <Card className="app-right">
+        <CardContent>
+         <h3>Live Cases by Country</h3>
+         <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
