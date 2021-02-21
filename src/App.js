66799e40183136ba3abfa60a214f02fb5e57e62b
Riph Kitsune <mitchell.mcshane1987@gmail.com>
2021-02-21 18:31:19 +0000

    cleaned up adding folders and made a Table.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core';
-import InfoBox from './InfoBox';
+import InfoBox from './InfoBox/InfoBox';
 import Map from './Map';
+import Table from './Table/Table';
 
 function App() {
 
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -30,6 +32,7 @@ function App() {
             value: country.countryInfo.iso2
           }));
 
+          setTableData(data);
           setCountries(countries);
         })
     }
@@ -84,6 +87,7 @@ function App() {
       <Card className="app-right">
         <CardContent>
           <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
         </CardContent>
       </Card>
