361b351c221daa6e8fb7d6fb1e7baae9cdd1d2ed
Riph Kitsune <mitchell.mcshane1987@gmail.com>
2021-02-21 17:01:52 +0000

    Adding onChange value for country

--- src/App.js
+++ src/App.js
@@ -6,32 +6,39 @@ import { MenuItem, FormControl, Select, } from '@material-ui/core';
 function App() {
 
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide")
 
   useEffect(() => {
 
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
-      .then((res) => res.json())
-      .then((data) => {
-        const countries = data.map((country) => ({
-          name: country.country,
-          value: country.countryInfo.iso2
-        }));
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
 
-        setCountries(countries);
-      })
+          setCountries(countries);
+        })
     }
     getCountriesData();
 
   }, [])
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    console.log("Hello",countryCode)
+  }
 
   return (
     <div className="App">
       <div className="app-header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
-
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
