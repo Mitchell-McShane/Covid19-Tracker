b7e701968c1483eb243b19e4f1807b62f93cf3c4
Riph Kitsune <mitchell.mcshane1987@gmail.com>
2021-03-03 22:09:58 +0000

    Details on map showing data when clicking the circles

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]); 
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -39,6 +40,7 @@ function App() {
 
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
         })
     }
@@ -91,7 +93,7 @@ function App() {
           <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
 
-        <Map center={mapCenter} zoom={mapZoom}/>
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom}/>
       </div>
       <Card className="app-right">
         <CardContent>
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,27 @@
+import React from 'react';
+import numeral from 'numeral';
+import { Circle, Popup } from 'react-leaflet';
+
+const casesTypeColors = {
+
+    cases: {
+        hex: "#CC1034",
+        multiplier: 200,
+    },
+    recovered: {
+        hex: "#7dd71d",
+        multiplier: 120,
+    },
+    deaths: {
+        hex: "#fb4443",
+        multiplier: 110,
+    },
+};
+
 export const sortData = (data) => {
     const sortedData = [...data];
 
-    sortedData.sort((a,b) => {
+    sortedData.sort((a, b) => {
         if (a.cases > b.cases) {
             return -1;
         } else {
@@ -10,3 +30,24 @@ export const sortData = (data) => {
     })
     return sortedData;
 }
+
+// Shows the circles on the map
+export const showDataOnMap = (data, casesType = "cases") => (
+    data.map(country => (
+        <Circle center={[country.countryInfo.lat, country.countryInfo.long]} fillOpacity={0.3}
+            color={casesTypeColors[casesType].hex} fillColor={casesTypeColors[casesType].hex}
+            radius={
+                Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+            }>
+            <Popup>
+            <div className="info-container">
+                <div className="info-flag" style={{ backgroundImage: `url(${country.countryInfo.flag})` }} />
+                <div className="info-name">{country.country}</div>
+                <div>Cases: {numeral(country.cases).format("0,0")}</div>
+                <div>Recovered: {numeral(country.recovered).format("0,0")}</div>
+                <div>Deaths: {numeral(country.deaths).format("0,0")}</div>
+            </div>
+            </Popup>
+        </Circle>
+    ))
+)
